e9e79939fd8b1793648de72e0892f0a8c93c9a36
Fabricio <fabriciooliveirads@hotmail.com>
2023-06-10 15:09:10 -0300

    Implementa paging source

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.example.core.domain.model.Character
+
 data class CharacterResponse(
     val id: String,
     val name: String,
     val thumbnailResponse: ThumbnailResponse
 )
+
+fun CharacterResponse.toCharacterModel(): Character {
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnailResponse.path}.${this.thumbnailResponse.extension}"
+    )
+}
--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,5 +1,9 @@
 package com.example.marvelapp.framework.network.response
 
+import androidx.compose.ui.unit.DpOffset
+
 data class DataContainerResponse(
+    val offset: Int,
+    val total: Int,
     val results : List<CharacterResponse>
 )
